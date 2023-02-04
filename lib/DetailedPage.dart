32a9bd0c0097667f2b9b1b10e72212c307f1e885
Redskull-127 <meerraja17@gmail.com>
2023-02-04 16:09:50 +0530

    Almost there

--- lib/DetailedPage.dart
+++ lib/DetailedPage.dart
@@ -1,15 +1,236 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:math';
 
-class DetailesPage extends StatelessWidget {
+class DetailesPage extends StatefulWidget {
+  final String ip;
+  final String vendor;
+  final String mac;
+  final time;
+  final String hostname;
+  const DetailesPage(
+      {super.key,
+      required this.ip,
+      required this.vendor,
+      required this.mac,
+      required this.time,
+      required this.hostname});
+
+  @override
+  State<DetailesPage> createState() => _DetailesPageState(
+      IP: ip, VENDOR: vendor, MAC: mac, TIME: time, HOSTNAME: hostname);
+}
+
+class _DetailesPageState extends State<DetailesPage> {
+  void stillScanning(BuildContext context) {
+    bool isScanning;
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Scanning'),
+            content: const Text('This might take a while'),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Cancel'))
+            ],
+          );
+        });
+  }
+
+  void searchedDone(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Vulnerable Ports'),
+            content: vulnArray.isEmpty
+                ? ListView(
+                    shrinkWrap: true,
+                    children: const [
+                      Text('No Vulnerable Ports Found'),
+                    ],
+                  )
+                : ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: vulnArray.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      if (vulnArray[index] == '80') {
+                        return Text('${vulnArray[index]} - HTTP');
+                      } else if (vulnArray[index] == '443') {
+                        return Text('${vulnArray[index]} - HTTPS');
+                      } else if (vulnArray[index] == '8080') {
+                        return Text('${vulnArray[index]} - HTTP');
+                      } else if (vulnArray[index] == '8443') {
+                        return Text('${vulnArray[index]} - HTTPS');
+                      } else if (vulnArray[index] == '445') {
+                        return Text('${vulnArray[index]} - SMB');
+                      } else if (vulnArray[index] == '139') {
+                        return Text('${vulnArray[index]} - SMB');
+                      } else if (vulnArray[index] == '137') {
+                        return Text('${vulnArray[index]} - SMB');
+                      } else if (vulnArray[index] == '53') {
+                        return Text('${vulnArray[index]} - DNS');
+                      } else if (vulnArray[index] == '25') {
+                        return Text('${vulnArray[index]} - SMTP');
+                      } else if (vulnArray[index] == '21') {
+                        return Text('${vulnArray[index]} - FTP');
+                      } else if (vulnArray[index] == '22') {
+                        return Text('${vulnArray[index]} - SSH');
+                      } else if (vulnArray[index] == '23') {
+                        return Text('${vulnArray[index]} - Telnet');
+                      } else if (vulnArray.isEmpty) {
+                        return const Text(
+                            'Your device is Secured\n No vulnerable ports found');
+                      }
+                    }),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Close'),
+              ),
+            ],
+          );
+        });
+  }
+
+  List<String> customArray = [
+    '20',
+    '21',
+    '22',
+    '23',
+    '25',
+    '53',
+    '137',
+    '139',
+    '445',
+    '80',
+    '443',
+    '8080',
+    '8443'
+  ];
+
+  List<String> vulnArray = [];
+
+  Future<void> pingPort(String host, int port) async {
+    print('running');
+    try {
+      final socket = await Socket.connect(host, port);
+      print('Connected to $host:$port');
+      vulnArray.add(port.toString());
+      await socket.close();
+    } catch (e) {
+      print('Failed to connect to $host:$port');
+    }
+  }
+
+  num roundToHour(int minutes) {
+    if (minutes < 60) {
+      return minutes;
+    } else {
+      return (minutes / 60);
+    }
+  }
+
+  final String IP;
+  final String VENDOR;
+  final String MAC;
+  final TIME;
+  final String HOSTNAME;
+  _DetailesPageState(
+      {required this.IP,
+      required this.VENDOR,
+      required this.MAC,
+      required this.TIME,
+      required this.HOSTNAME});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Detail Page - SSID"),
-      ),
-      body: const Center(
-        child: Text("This is the detail page for SSID"),
-      ),
-    );
+    if (IP != '') {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("$IP - $VENDOR"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ListTile(
+                title: const Text('IP Address :'),
+                subtitle: Text('$IP'),
+              ),
+              ListTile(
+                title: const Text('Mac Address :'),
+                subtitle: Text('$MAC'),
+              ),
+              ListTile(
+                title: const Text('Connected Since :'),
+                subtitle: Text('${roundToHour(TIME).toStringAsFixed(2)} Hours'),
+              ),
+              ListTile(
+                title: const Text('Hostname :'),
+                subtitle: Text('$HOSTNAME'),
+              ),
+              ListTile(
+                title: const Text('Vendor :'),
+                subtitle: Text('$VENDOR'),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Container(
+          height: 60,
+          width: 140,
+          child: FloatingActionButton(
+            onPressed: () {
+              vulnArray = [];
+              for (var i = 0; i < customArray.length; i++) {
+                pingPort(IP, int.parse(customArray[i]));
+              }
+
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Row(children: const [
+                    CircularProgressIndicator(),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text('Scanning Ports ... (Takes 10 Seconds))')
+                  ]),
+                  duration: Duration(seconds: 10),
+                ),
+              );
+
+              Future.delayed(Duration(seconds: 10), () {
+                searchedDone(context);
+              });
+            },
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(Icons.wifi),
+                  Text(
+                    'Scan Ports',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ]),
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("$IP - $VENDOR"),
+        ),
+        body: const Center(child: CircularProgressIndicator.adaptive()),
+      );
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:arp_scanner/arp_scanner.dart';
 import 'package:arp_scanner/device.dart';
 import 'DetailedPage.dart';
+import 'package:http/http.dart' as http;
+import 'AllowBadCRT.dart';
 
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
 
@@ -14,7 +18,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Wide',
-      theme: ThemeData(useMaterial3: true, brightness: Brightness.dark),
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+      ),
       home: const MyHomePage(title: 'Wide'),
     );
   }
@@ -31,9 +38,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String _result = '';
-  bool _isScanning = true;
   final scaffoldKey = GlobalKey<ScaffoldState>();
-  // array to add device results
   List<Device> _devices = [];
   @override
   void initState() {
@@ -47,7 +52,6 @@ class _MyHomePageState extends State<MyHomePage> {
     });
     ArpScanner.onScanFinished.listen((List<Device> devices) {
       setState(() {
-        // _result = "${_result}total: ${devices.length}";
         _devices = devices;
       });
       // show snackbar
@@ -57,63 +61,172 @@ class _MyHomePageState extends State<MyHomePage> {
     //show snackbar
   }
 
+  Future<http.Response> fetchData() async {
+    final response = await http.get(Uri.parse(
+        'https://api.quotable.io/random?tags=technology&maxLength=35'));
+    return response;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.cancel),
-            onPressed: () async {
-              await ArpScanner.cancel();
-            },
+    if (_result != '' && _result != null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'All Devices',
+            style: TextStyle(height: 20),
           ),
-        ],
-        title: Text(widget.title),
-      ),
-      // map the array and print each device
-      body: ListView.builder(
-        itemCount: _devices.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.only(top: 10),
-            child: ListTile(
-              title: Text(_devices[index].ip.toString()),
-              subtitle:
-                  Text("${_devices[index].mac} ${_devices[index].vendor}"),
-              tileColor: Colors.amber[700],
-              //add icon
-              leading: const Icon(
-                Icons.devices,
-                color: Colors.white,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: Text(
+                'Total: ${_devices.length}',
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
               ),
-              //add icon at end
-              trailing: const Icon(
-                Icons.arrow_forward_ios,
-                color: Colors.white,
+            )
+          ],
+        ),
+        // map the array and print each device
+        body: _devices.length > 0
+            ? ListView.builder(
+                itemCount: _devices.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: ListTile(
+                      title: Text(
+                        _devices[index].ip.toString(),
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      subtitle: Text(
+                        "${_devices[index].mac} ${_devices[index].vendor}",
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      tileColor: Colors.purple[900],
+                      //add icon
+                      leading: const Icon(
+                        Icons.devices,
+                        color: Colors.white,
+                      ),
+                      //add icon at end
+                      trailing: const Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.white,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      onTap: () => {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => DetailesPage(
+                                    ip: _devices[index].ip.toString(),
+                                    mac: _devices[index].mac.toString(),
+                                    vendor: _devices[index].vendor.toString(),
+                                    time: _devices[index].time.toInt(),
+                                    hostname:
+                                        _devices[index].hostname.toString())))
+                      },
+                    ),
+                  );
+                },
+              )
+            : const Center(
+                child: CircularProgressIndicator.adaptive(),
               ),
+
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.grey[500],
+            onPressed: () async {
+              //scan sub net devices
+              await ArpScanner.scan();
+              setState(() {
+                _result = "";
+              });
+              // add snackbar with total numbers
+            },
+            child: const Icon(Icons.refresh)),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+            toolbarHeight: 127,
+            centerTitle: true,
+            title: Image.asset('assets/Logo/logo.png', height: 143)),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to Wide',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30),
+            const Text('Top things to do here!',
+                style: const TextStyle(fontSize: 16)),
+            const SizedBox(height: 10),
+            ListTile(
+              title: const Text('Connect to WiFi'),
+              subtitle: const Text('For scanning all devices in your network'),
+              tileColor: Colors.purple[100],
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10),
               ),
-              onTap: () => {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => DetailesPage()))
-              },
             ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.scanner_sharp),
-          onPressed: () async {
-            //scan sub net devices
-            await ArpScanner.scan();
-            setState(() {
-              _result = "";
-            });
-            // add snackbar with total numbers
-          }),
-    );
+            const SizedBox(height: 10),
+            ListTile(
+              title: const Text('Click the button below'),
+              subtitle: const Text('It will scan all your devices'),
+              tileColor: Colors.purple[100],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            const SizedBox(height: 10),
+            ListTile(
+              title: const Text('Search for vulnerable ports'),
+              subtitle: const Text('I will make it sure you :)'),
+              tileColor: Colors.purple[100],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            const SizedBox(height: 10),
+            ListTile(
+              title: const Text('Take Immediate Actions'),
+              subtitle: const Text(
+                  'if you find any vulnerable ports or any unknown device, Take immediate actions'),
+              tileColor: Colors.purple[100],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ],
+        )),
+        floatingActionButton: Container(
+          height: 60,
+          width: 140,
+          child: FloatingActionButton(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [Icon(Icons.wifi), Text('Scan Devices')]),
+              onPressed: () async {
+                await ArpScanner.scan();
+                setState(() {
+                  _result = "";
+                });
+                // add snackbar with total numbers
+              }),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      );
+    }
   }
 }
